964e9c5e4d9f6105cae12cec45a8d2129bb5144f
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-21 10:35:59 +0000

    Reports: added viewmodel & template pair to render the list of donations.

---
+++ src/viewmodels/report.js
@@ -0,0 +1,13 @@
+import {inject} from 'aurelia-framework';
+import DonationService from '../services/donation-service';
+
+@inject(DonationService)
+export class Report {
+
+  donations = [];
+
+  constructor(ds) {
+    this.donationService = ds;
+    this.donations = this.donationService.donations;
+  }
+}
