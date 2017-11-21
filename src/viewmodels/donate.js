8c81373c8b1748ef401cef55e32605376ab1c54e
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-21 10:23:58 +0000

    Binding: Adding candidates.

--- src/viewmodels/donate.js
+++ src/viewmodels/donate.js
@@ -4,8 +4,21 @@ export class Donate {
   methods = ['Cash', 'PayPal'];
   selectedMethod = 'Cash';
 
+  candidates = [
+    {
+      firstName: 'Lisa',
+      lastName: 'Simpson'
+    },
+    {
+      firstName: 'Bart',
+      lastName: 'Simpson'
+    }
+  ];
+  selectedCandidate = this.candidates[0];
+
   makeDonation() {
     console.log(`Amount = ${this.amount}`);
     console.log(`Method = ${this.selectedMethod}`);
+    console.log(`Candidate = ${this.selectedCandidate.firstName} ${this.selectedCandidate.lastName}`);
   }
 }
