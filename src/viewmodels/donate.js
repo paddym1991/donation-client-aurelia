4cbb0da06ad95398819b51d575b998a7cd739291
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-21 10:22:30 +0000

    Binding: adding payment method.

--- src/viewmodels/donate.js
+++ src/viewmodels/donate.js
@@ -1,8 +1,11 @@
 export class Donate {
 
   amount = 5;
+  methods = ['Cash', 'PayPal'];
+  selectedMethod = 'Cash';
 
   makeDonation() {
     console.log(`Amount = ${this.amount}`);
+    console.log(`Method = ${this.selectedMethod}`);
   }
 }
