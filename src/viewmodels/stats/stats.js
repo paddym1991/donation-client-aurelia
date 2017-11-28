3705fc0be61dc0817a25ef4d9b7862d5c1142b48
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-28 15:13:37 +0000

    Adjust stats view model to attach to dom.

--- src/viewmodels/stats/stats.js
+++ src/viewmodels/stats/stats.js
@@ -1,15 +1,27 @@
+/**
+ * This is a revised implementation of stats.js, which handles the attached event.
+ * This is triggered when the component becomes active.
+ * We retrieve the current value from the donation-service at this this point.
+ * In particular, the attached event/method.
+ */
 import {inject} from 'aurelia-framework';
 import {TotalUpdate} from '../../services/messages';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import DonationService from '../../services/donation-service';
 
-@inject(EventAggregator)
+@inject(EventAggregator, DonationService)
 export class Stats {
 
   total = 0;
 
-  constructor(ea) {
+  constructor(ea, ds) {
+    this.ds = ds;
     ea.subscribe(TotalUpdate, msg => {
       this.total = msg.total;
     });
+  }
+
+  attached() {
+    this.total = this.ds.total;
   }
 }
