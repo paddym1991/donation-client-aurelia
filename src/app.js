bc8d942de63c682074ee82cae6ad681e6556efdc
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-28 10:38:08 +0000

    bind to login view in app.js.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+
+  email = 'marge@simpson.com';
+  password = 'secret';
+
+  loggedIn = false;
+
+  login() {
+    console.log(`Logging in ${this.email}`);
+    this.loggedIn = true;
   }
 }
