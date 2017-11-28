801e7640e9a63ca024b5a56433421d9a89b15391
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-28 14:55:45 +0000

    Introduce routers - one for a logged out status (app) and one for a logged in status.

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-import {inject} from 'aurelia-framework';
+import {inject, Aurelia} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
-import DonationService from './services/donation-service';
 import {LoginStatus} from './services/messages';
 
-@inject(EventAggregator, DonationService)
+@inject(Aurelia, EventAggregator)
 export class App {
 
-  loggedIn = false;
-  showSignup = false;
-
-  constructor(ea, ds) {
-    this.donationService = ds;
+  constructor(au, ea) {
     ea.subscribe(LoginStatus, msg => {
-      this.loggedIn = msg.status.success;
+      if (msg.status.success === true) {
+        au.setRoot('home').then(() => {
+          this.router.navigateToRoute('donate');
+        });
+      } else {
+        au.setRoot('app').then(() => {
+          this.router.navigateToRoute('login');
+        });
+      }
     });
   }
 
-  signup() {
-    this.showSignup = true;
-  }
-
-  logout() {
-    console.log('Logging out`');
-    this.loggedIn = false;
+  configureRouter(config, router) {
+    config.map([
+      { route: ['', 'login'], name: 'login', moduleId: 'viewmodels/login/login', nav: true, title: 'Login' },
+      { route: 'signup', name: 'signup', moduleId: 'viewmodels/signup/signup', nav: true, title: 'Signup' }
+    ]);
+    this.router = router;
   }
 }
---
+++ src/home.js
@@ -0,0 +1,24 @@
+import { inject, Aurelia } from 'aurelia-framework';
+
+@inject(Aurelia)
+export class Home {
+
+  constructor(au) {
+    this.aurelia = au;
+  }
+
+  configureRouter(config, router) {
+    config.map([
+      { route: ['', 'home'], name: 'donate', moduleId: 'viewmodels/donate/donate', nav: true, title: 'Donate' },
+      { route: 'report', name: 'report', moduleId: 'viewmodels/report/report', nav: true, title: 'Report' },
+      { route: 'candidates', name: 'candidates', moduleId: 'viewmodels/candidates/candidates', nav: true, title: 'Candidates' },
+      { route: 'stats', name: 'stats', moduleId: 'viewmodels/stats/stats', nav: true, title: 'Stats' },
+      { route: 'logout', name: 'logout', moduleId: 'viewmodels/logout/logout', nav: true, title: 'Logout' }
+    ]);
+    this.router = router;
+
+    config.mapUnknownRoutes(instruction => {
+      return 'home';
+    });
+  }
+}
