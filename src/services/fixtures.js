cd39dec99c8d1384176f508e8c8f77b20013d4eb
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-04 09:56:30 +0000

    Removed cached data from fixtures + introduce base url for remote server.

--- src/services/fixtures.js
+++ src/services/fixtures.js
@@ -1,43 +1,5 @@
 export default class Fixtures {
 
+  baseUrl = 'http://localhost:4000';
   methods = ['Cash', 'PayPal'];
-
-  candidates = [
-    {
-      firstName: 'Lisa',
-      lastName: 'Simpson'
-    },
-    {
-      firstName: 'Bart',
-      lastName: 'Simpson'
-    }
-  ];
-
-  donations = [
-    {
-      amount: 23,
-      method: 'cash',
-      candidate: this.candidates[0]
-    },
-    {
-      amount: 212,
-      method: 'paypal',
-      candidate: this.candidates[1]
-    }
-  ];
-
-  users = {
-    'homer@simpson.com': {
-      firstName: 'Homer',
-      lastName: 'Simpson',
-      email: 'homer@simpson.com',
-      password: 'secret'
-    },
-    'marge@simpson.com': {
-      firstName: 'Marge',
-      lastName: 'Simpson',
-      email: 'marge@simpson.com',
-      password: 'secret'
-    }
-  }
 }
