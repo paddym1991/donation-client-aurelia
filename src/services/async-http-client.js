00b29b265626bb23ad20b5e9dda1942499e44193
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-05 10:38:33 +0000

    Include JWT authentication via authenticate feature.

--- src/services/async-http-client.js
+++ src/services/async-http-client.js
@@ -1,18 +1,57 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 import Fixtures from './fixtures';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {LoginStatus} from './messages';
 
 /**
  * This is a new class, which will encapsulate access to the aurelia-http-client we installed in the last step
  */
 
-@inject(HttpClient, Fixtures)
+@inject(HttpClient, Fixtures, EventAggregator)
 export default class AsyncHttpClient {
 
-  constructor(httpClient, fixtures) {
+  constructor(httpClient, fixtures, ea) {
     this.http = httpClient;
     this.http.configure(http => {
       http.withBaseUrl(fixtures.baseUrl);
+    });
+    this.ea = ea;
+  }
+
+  /**
+   * introduce a new method to authenticate a user to the api.
+   * This (invoked in the next step from donation-service),
+   * will post to an authenticate url - and if successful,
+   * attaches the returned token to all subsequent invocations.
+   * It will also store the token in local storage in case our app is suspended
+   * @param url
+   * @param user
+   */
+  authenticate(url, user) {
+    this.http.post(url, user).then(response => {
+      const status = response.content;
+      if (status.success) {
+        this.http.configure(configuration => {
+          configuration.withHeader('Authorization', 'bearer ' + response.content.token);
+        });
+      }
+      this.ea.publish(new LoginStatus(status));
+    }).catch(error => {
+      const status = {
+        success: false,
+        message: 'service not available'
+      };
+      this.ea.publish(new LoginStatus(status));
+    });
+  }
+
+  /**
+   * Clear method to accompany the authenticate method
+   */
+  clearAuthentication() {
+    this.http.configure(configuration => {
+      configuration.withHeader('Authorization', '');
     });
   }
 
